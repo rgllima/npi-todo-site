5a13c2ba7d6b1fc9de30302507e35bf6b0158b5c
Rafael Lima <rglllima@gmail.com>
2020-03-10 11:58:45 -0300

    Ciração da página de Login

---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login">
+    <v-container fluid>
+      <v-row>
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-text-field v-model="user.email" label="Email"></v-text-field>
+        </v-col>
+
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-text-field v-model="user.password" label="Email"></v-text-field>
+        </v-col>
+
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-button>Sign In</v-button>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: { email: "", password: "" }
+    };
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.login
+</style>
